79663b0808a51f15f8912a21e1cbb11e0bf4d4b9
Matthew Brooks <matthew.brooks@bbc.co.uk>
2013-06-24 10:45:18 +0100

    Latest

--- Subtitles.js
+++ Subtitles.js
@@ -223,19 +223,6 @@ define(["BoundingBox2D"], function(BoundingBox2D) {
 		return result;
 	};
 
-	function renderBounds(ctx, span, w, h)
-	{
-		var eyeTrackingErr = {x:0.015104194253236364, y:0.026851900894642428};
-		var readUnder = 0.06;
-		ctx.lineWidth = 1;
-		var b = span.position.bounds;
-
-		ctx.strokeStyle = "white";
-		ctx.rect((b.minx - eyeTrackingErr.x) * w, (b.miny - eyeTrackingErr.y) * h, ((b.maxx - b.minx) + (eyeTrackingErr.x*2)) * w, ((b.maxy - b.miny) + (readUnder + (eyeTrackingErr.y*2))) * h);
-		ctx.stroke();
-		ctx.strokeStyle = "black";
-	}
-
 	Subtitles.prototype.renderAtDefaultPosition = function (elem, canvas, lineHeight, relativeSize) {
 
 		var ctx = canvas.getContext('2d');
@@ -308,7 +295,6 @@ define(["BoundingBox2D"], function(BoundingBox2D) {
 				var lineY = y + (lineHeight * currentLine);
 				ctx.strokeText(span.text, x, lineY);
 				ctx.fillText(span.text, x, lineY);
-//				renderBounds(ctx, span, canvas.width, canvas.height);
 				currentLine++;
 			});
 		}
@@ -372,8 +358,6 @@ define(["BoundingBox2D"], function(BoundingBox2D) {
 			ctx.lineWidth = 5;
 			ctx.strokeText(span.text, span.position.x * canvas.width, span.position.y * canvas.height);
 			ctx.fillText(span.text, span.position.x * canvas.width, span.position.y * canvas.height);
-
-			renderBounds(ctx, span, canvas.width, canvas.height);
 		});		
 	};
 
